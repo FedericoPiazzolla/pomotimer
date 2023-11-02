4ba51b52456c43def1b1f774f5184e8f331ecb34
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-02 18:13:49 +0100

    add: wip on play button for start timer

--- js/script.js
+++ js/script.js
@@ -3,16 +3,50 @@ const breakBtn = document.getElementById("break");
 
 const pauseBtn = document.getElementById("pause");
 const playBtn = document.getElementById("play");
-const restartBTn = document.getElementById("restart");
 
 const audio = new Audio(`allarm.mp3`);
 
-let focusTimer = 25; 
+let focusTimer = 24; 
 let breakTimer = 5;
 let seconds = 60;
 
-/*****************************************/
-// function for decrement seconds
-let remaninTimer = () =>{
+const elemMinutes = document.getElementById("minutes");
+const elemSeconds = document.getElementById("seconds");
+/*******************************************************************/
+
+// logica del programma
+focusBtn.addEventListener("click", function(){
+  elemSeconds.innerHTML = "00";
+  minutes.innerHTML = "25";
+
+  focusBtn.classList.add("opacity");
+  breakBtn.classList.remove("opacity");
   
-}
+  playBtn.addEventListener("click", function(){
+    const myInterval = setInterval(function(){
+      seconds--;
+
+      if(seconds === 0) {
+        focusTimer = focusTimer - 1;
+
+        if(focusTimer === - 1) {
+          clearInterval(remaninTimer);
+        };
+
+        seconds= 60;
+      };
+
+      elemSeconds.innerHTML = seconds;
+      elemMinutes.innerHTML = focusTimer;
+
+    }, 1000);
+  });
+});
+
+breakBtn.addEventListener("click", function(){
+  elemSeconds.innerHTML = "00";
+  minutes.innerHTML = "05";
+
+  breakBtn.classList.add("opacity");
+  focusBtn.classList.remove("opacity");
+});
