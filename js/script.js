4b9d024cafa91b4c46625c1e3c0c45bb69364476
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-02 13:32:32 +0100

    add: variable btn on js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let focusBtn = document.getElementById("focus");
+let breakBtn = document.getElementById("break");
+
+let pauseBtn = document.getElementById("pause");
+let playBtn = document.getElementById("play");
+let restartBTn = document.getElementById("restart");
+
